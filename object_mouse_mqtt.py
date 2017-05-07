c2eac2c16191c90b796773980cd368ef6ffd8947
Lucas Cristiano Calixto Dantas <lucascristiano27@gmail.com>
2017-05-06 23:30:20 -0300

    Code refactoring

--- object_mouse_mqtt.py
+++ object_mouse_mqtt.py
@@ -1,42 +1,24 @@
-# USAGE
-# python object_movement.py --video object_tracking_example.mp4
-# python object_movement.py
-
-# import the necessary packages
-from collections import deque
-from threading import Timer
-import numpy as np
 import argparse
-import imutils
-import cv2
-import pyautogui
-import paho.mqtt.client as mqtt
 import json
-from PyQt4 import QtGui # to get display resolution
+from threading import Timer
+
+import cv2
+import imutils
+import numpy as np
+import paho.mqtt.client as mqtt
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=32,
-	help="max buffer size")
-ap.add_argument("-q","--mqtt", nargs='?',
-	help="enable mqtt with given ip")
-args = vars(ap.parse_args())
+ap.add_argument("-q", "--mqtt", nargs='?', default='', help="enable mqtt with given ip")
+ap.add_argument("-i", "--showimage", action='store_true', help="enable image show")
+args = ap.parse_args()
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space
+# define the lower and upper boundaries of the "green" ball in the HSV color space
 GREEN_LOWER = (29, 86, 6)
 GREEN_UPPER = (64, 255, 255)
 
-# get display resolution
-app = QtGui.QApplication([])
-SCREEN_RESOLUTION = app.desktop().screenGeometry()
-SCREEN_WIDTH, SCREEN_HEIGHT = SCREEN_RESOLUTION.width(), SCREEN_RESOLUTION.height()
 FRAME_WIDTH, FRAME_HEIGHT = 352, 240
-MOUSE_THRESHOLD = 20
-WIDTH_FACTOR = np.abs(SCREEN_WIDTH / FRAME_WIDTH)
-HEIGHT_FACTOR = np.abs(SCREEN_HEIGHT / FRAME_HEIGHT)
+POSITION_THRESHOLD = 5
 
 # Limits
 EAST_LIMIT = 0.9 * FRAME_WIDTH
@@ -45,193 +27,152 @@ NORTH_LIMIT = 0.1 * FRAME_HEIGHT
 SOUTH_LIMIT = 0.7 * FRAME_HEIGHT
 TIMER_LIMIT = 1.0
 
-# initialize the list of tracked points, the frame counter,
-# and the coordinate deltas
-pts = deque(maxlen=args["buffer"])
-last_mouse_pos = (0, 0)
-counter = 0
-(dX, dY) = (0, 0)
-direction = ""
-
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-	camera = cv2.VideoCapture(0)
-	# camera.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
-	# camera.set(cv2.CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT)
-	# camera.set(cv2.CAP_PROP_FPS,15)
-
-# otherwise, grab a reference to the video file
-else:
-	camera = cv2.VideoCapture(args["video"])
-
-mqtt_enabled = args.get("mqtt",False)
+# initialize the last position, direction string and position and direction flags
+last_position = (0, 0)
+lost_position = False
+direction_str = ""
 direction_sent = False
+
+camera = cv2.VideoCapture(0)
+# camera.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+# camera.set(cv2.CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT)
+# camera.set(cv2.CAP_PROP_FPS,15)
+
+mqtt_enabled = True if args.mqtt else False
 
 # Connects to mqtt broker
 if mqtt_enabled:
-	client = mqtt.Client()
-	client.connect(args["mqtt"], 1883)
-	client.loop_start()
+    print('MQTT enabled')
+    mqtt_host = args.mqtt
+    mqtt_port = 1883
+    print('Connecting to {} on port {}.'.format(mqtt_host, mqtt_port))
+    client = mqtt.Client()
+    client.connect(mqtt_host, mqtt_port)
+    client.loop_start()
+    print('Connected!')
+
 
 def mqtt_publish(topic, payload):
-	if mqtt_enabled:
-		client.publish(topic, payload)
-		print('(mqtt enabled)'+topic + ' - ' + payload)
-	else:
-		print(topic + ' - ' + payload)
+    if mqtt_enabled:
+        client.publish(topic, payload)
+        print('(MQTT) {} - {}'.format(topic, payload))
+    else:
+        print(topic + ' - ' + payload)
+
 
 def reset_direction():
-	global direction_sent
-	direction_sent = False
+    global direction_str
+    global direction_sent
+    direction_str = ""
+    direction_sent = False
+
 
 def send_direction(direction):
-	global direction_sent
-	direction_sent = True
-	mqtt_publish('direction',direction)
-	t = Timer(TIMER_LIMIT, reset_direction)
-	t.start()
+    global direction_sent
+    direction_sent = True
+    mqtt_publish('direction', direction)
+    t = Timer(TIMER_LIMIT, reset_direction)
+    t.start()
+
+
+def send_position(position):
+    if position:
+        x, y = position
+        position_payload = json.dumps({'x': x, 'y': y})
+    else:
+        position_payload = 'none'
+
+    mqtt_publish('position', position_payload)
+
 
 # keep looping
-while True:
-	# grab the current frame
-	(grabbed, frame) = camera.read()
+try:
+    while True:
+        # grab the current frame
+        (grabbed, frame) = camera.read()
 
-	# if we are viewing a video and we did not grab a frame,
-	# then we have reached the end of the video
-	if args.get("video") and not grabbed:
-		break
+        # resize the frame, blur it, and convert it to the HSV color space
+        frame = imutils.resize(frame, width=FRAME_WIDTH, height=FRAME_HEIGHT)
+        # mirror image horizontally
+        frame = cv2.flip(frame, 1)
+        # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# resize the frame, blur it, and convert it to the HSV
-	# color space
-	frame = imutils.resize(frame, width=FRAME_WIDTH, height=FRAME_HEIGHT)
-	# mirror image horizontally
-	frame = cv2.flip(frame, 1)
-	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # construct a mask for the color "green", then perform a series of dilations and erosions to remove any small
+        # blobs left in the mask
+        mask = cv2.inRange(hsv, GREEN_LOWER, GREEN_UPPER)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, GREEN_LOWER, GREEN_UPPER)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
+        # find contours in the mask and initialize the current (x, y) center of the ball
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        center = None
 
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
+        # only proceed if at least one contour was found
+        if len(contours) > 0:
+            lost_position = False
 
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            # find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
+            c = max(contours, key=cv2.contourArea)
+            ((circle_x, circle_y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			# cv2.circle(frame, (int(x), int(y)), int(radius),
-			# 	(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), 1)
-			pts.appendleft(center)
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame, then update the list of tracked points
+                cv2.circle(frame, (int(circle_x), int(circle_y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), 1)
 
-		center_x = center[0]
-		center_y = center[1]
+            center_x, center_y = center
 
-		if not direction_sent:
-			if center_x > EAST_LIMIT:
-				send_direction('EAST');
-			elif center_x < WEST_LIMIT:
-				send_direction('WEST')
-			elif center_y < NORTH_LIMIT:
-				send_direction('NORTH')
-			elif center_y > SOUTH_LIMIT:
-				send_direction('SOUTH')
-			# else:
-			# 	print('limit=%d x=%d y=%d' % (SOUTH_LIMIT,center_x,center_y))
+            if not direction_sent:
+                if center_x > EAST_LIMIT:
+                    direction_str = 'EAST'
+                    send_direction(direction_str)
+                elif center_x < WEST_LIMIT:
+                    direction_str = 'WEST'
+                    send_direction(direction_str)
+                elif center_y < NORTH_LIMIT:
+                    direction_str = 'NORTH'
+                    send_direction(direction_str)
+                elif center_y > SOUTH_LIMIT:
+                    direction_str = 'SOUTH'
+                    send_direction(direction_str)
+                else:
+                    direction_str = ''
 
-	else:
-		mqtt_publish('position', 'none')
+            position_dX = np.abs(last_position[0] - center_x)
+            position_dY = np.abs(last_position[1] - center_y)
 
-	# loop over the set of tracked points
-	for i in np.arange(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-		if pts[i - 1] is None or pts[i] is None:
-			continue
+            if position_dX > POSITION_THRESHOLD or position_dY > POSITION_THRESHOLD:
+                last_position = (center_x, center_y)
+                send_position(last_position)
 
-		# check to see if enough points have been accumulated in
-		# the buffer
-		if counter >= 10 and i == 1 and len(pts) >= 10 and pts[-10] is not None:
-			# compute the difference between the x and y
-			# coordinates and re-initialize the direction
-			# text variables
-			dX = pts[-10][0] - pts[i][0]
-			dY = pts[-10][1] - pts[i][1]
-			(dirX, dirY) = ("", "")
+        else:
+            if not lost_position:
+                send_position(None)
+                lost_position = True
 
-			center_x = pts[i][0]
-			center_y = pts[i][1]
+        if args.showimage:
+            # show the movement deltas and the direction of movement on the frame
+            cv2.putText(frame, direction_str, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 3)
+            cv2.putText(frame, "x: {}, y: {}".format(last_position[0], last_position[1]), (10, frame.shape[0] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-			mouse_dX = np.abs(last_mouse_pos[0] - center_x)
-			mouse_dY = np.abs(last_mouse_pos[1] - center_y)
+            # show the frame to our screen
+            cv2.imshow("Frame", frame)
 
-			if mouse_dX > MOUSE_THRESHOLD or mouse_dY > MOUSE_THRESHOLD:
-				last_mouse_pos = (center_x, center_y)
+            # if the 'q' key is pressed, stop the loop
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                if mqtt_enabled:
+                    client.disconnect()
+                break
 
-				abs_center_x = np.round(center_x * WIDTH_FACTOR)
-				abs_center_y = np.round(center_y * HEIGHT_FACTOR)
-				# pyautogui.moveTo(abs_center_x, abs_center_y, duration=0)
-				position_json = json.dumps({'x': abs_center_x, 'y': abs_center_y})
-
-				mqtt_publish('position', position_json)
-
-			# ensure there is significant movement in the
-			# x-direction
-			if np.abs(dX) > 50:
-				dirX = "East" if np.sign(dX) == 1 else "West"
-
-			# ensure there is significant movement in the
-			# y-direction
-			if np.abs(dY) > 50:
-				dirY = "North" if np.sign(dY) == 1 else "South"
-
-			# handle when both directions are non-empty
-			if dirX != "" and dirY != "":
-				direction = "{}-{}".format(dirY, dirX)
-
-			# otherwise, only one direction is non-empty
-			else:
-				direction = dirX if dirX != "" else dirY
-
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-		# thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-		# cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-	# show the movement deltas and the direction of movement on
-	# the frame
-	cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-		0.65, (0, 0, 255), 3)
-	cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-		(10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
-
-	# show the frame to our screen and increment the frame counter
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-	counter += 1
-
-	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+except KeyboardInterrupt:
+    if mqtt_enabled:
+        client.disconnect()
 
 # cleanup the camera and close any open windows
 camera.release()
